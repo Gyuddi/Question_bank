835518a1525c309d10dbb91fca885a8c66ef3931
Gyuddi <samll8539@naver.com>
2022-01-24 23:22:23 +0900

    new file

--- src/Q15596.java
+++ src/Q15596.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Q15596 {
+public class Q15596 {
+    long sum(int[] a) {
+        long a_sum = 0;
+        for(int num:a){
+            a_sum += num;
+        }
+        return 0;
+    }
 }
--- src/Q4673.java
+++ src/Q4673.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class Q4673 {
+public class Q4673 {
 }
