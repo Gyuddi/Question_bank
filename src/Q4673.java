ee2dfddec324ad77ae7b3f26615252309c2c65f3
Gyuddi <samll8539@naver.com>
2022-01-24 23:46:46 +0900

    new file

--- src/Q4673.java
+++ src/Q4673.java
@@ -1,2 +1,27 @@
 public class Q4673 {
+    public static int get_sn(int a){
+        int result = a;
+        while (a > 0){
+            int rest = a % 10;
+            a = a/10;
+            result += rest;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        boolean[] pool = new boolean[10000];
+        for(int i = 0;i<10000;i++){
+            int number = get_sn(i);
+            if (number >= 10000){
+                continue;
+            }
+            pool[number] = true;
+        }
+        for(int i = 0;i<10000;i++){
+            if(pool[i] == false){
+                System.out.println(i);
+            }
+        }
+    }
+
 }
