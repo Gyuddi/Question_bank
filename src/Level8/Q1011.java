cade2041aa7041489b26531439d9a643203dab92
Gyuddi <samll8539@naver.com>
2022-01-29 22:26:50 +0900

    new file

---
+++ src/Level8/Q1011.java
@@ -0,0 +1,18 @@
+package Level8;
+
+import java.util.Scanner;
+
+public class Q1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        for(int i = 0;i<total;i++){
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            double dis = end - start;
+            double sqrt = Math.round(Math.sqrt(dis));
+
+
+        }
+    }
+}
--- src/Level8/Q2839.java
+++ src/Level8/Q2839.java
@@ -1,4 +1,42 @@
 package Level8;
 
+import java.util.Scanner;
+
 public class Q2839 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 0;
+        if(num%5 == 0){
+            count = num/5;
+        }
+        else if(num % 5 == 1){
+            if(num == 1){
+                count = -1;
+            }
+            else {
+                count = (num/5-1) +2;
+            }
+        }
+        else if(num % 5 == 2){
+            if(num == 7 || num == 2){
+                count = -1;
+            }
+            else {
+                count = (num/5-2)+4;
+            }
+        }
+        else if(num % 5 == 3){
+            count = (num/5)+1;
+        }
+        else if (num % 5 == 4){
+            if(num == 4 || num == 8){
+                count = -1;
+            }
+            else {
+                count = (num/5-1)+3;
+            }
+        }
+        System.out.println(count);
+    }
 }
